f51bb9d0542952a905d63e507fb32a0dbd00fe99
yojoe <yousefsuleiman10@gmail.com>
2025-01-14 15:38:34 -0600

    sync

--- Defaults.cs
+++ Defaults.cs
@@ -6,7 +6,7 @@ namespace QuranCli
     internal static class Defaults
     {
         public static readonly string applicationName = "Quran CLI";
-        public static readonly string configurationPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "quran");
+        public static readonly string configurationPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "quran-cli");
         public static readonly string databaseFileName = "data.db";
         public static readonly string resourceUrl = "https://yojoecapital.github.io/quran-cli";
         public static readonly string ayatFileName = "verses.txt";
